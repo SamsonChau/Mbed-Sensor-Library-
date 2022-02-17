689bd7f63a4a55d0d2a19d801a2d3125bcd3fd11
SChau <71276205+SamsonChau@users.noreply.github.com>
2022-02-17 09:49:58 +0800

    Update main.cpp

--- Encoder/Incrimental ABI Encoder/main.cpp
+++ Encoder/Incrimental ABI Encoder/main.cpp
@@ -1,7 +1,5 @@
 #include "mbed.h"
 #include "QEI.h"
-#include <geometry_msgs/Point.h>
-#include <ros.h>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -16,11 +14,9 @@
 #define EncoderY_A PB_6
 #define EncoderY_B PB_1
 
-const int Encoder_PPR = 2048;
-const float Encoder_Omni_Wheel_Radius = 0.0254; // Radius of the Omni Wheel
-const float omni_wheel = 2 * M_PI * Encoder_Omni_Wheel_Radius;
-
-QEI EncoderX(EncoderX_A, EncoderX_B, NC, Encoder_PPR);
+// Use AMT102V encoder as an example 
+const int Encoder_PPR = 2048;                           //setup the pulse per rotation of the encoder 
+QEI EncoderX(EncoderX_A, EncoderX_B, NC, Encoder_PPR);  
 QEI EncoderY(EncoderY_A, EncoderY_B, NC, Encoder_PPR);
 
 using namespace std::chrono;
@@ -30,25 +26,24 @@ Timer t1;
 int dt = 0;
 int loopTime = 5;       // 10 milliseconds
 
-ros::NodeHandle nh;
-geometry_msgs::Point Encoder_msg;
-
-ros::Publisher Encoder_publisher("encoder/position", &Encoder_msg);
-
 int main() {
-  nh.getHardware()->setBaud(128000); // Maximum for f303K8
-  nh.initNode();
-  nh.advertise(Encoder_publisher);
+  int Encoder_x = 0;
+  int Encoder_y = 0;
+  int Encoder_vx = 0;
+  int Encoder_vy = 0;
   t1.start();
-  while (true) {
+  while (true){
     dt = duration_cast<milliseconds>(t1.elapsed_time()).count();
     if (dt >= loopTime) { // run a loop every 10ms
       t1.stop();
-      // calculat the Encoder distance in mm
-      Encoder_msg.x = ((float)EncoderX.getPulses() * omni_wheel / Encoder_PPR) ;
-      Encoder_msg.y = ((float)EncoderY.getPulses() * omni_wheel / Encoder_PPR) ;
-      Encoder_publisher.publish(&Encoder_msg);
-      nh.spinOnce();
+      // calculate the Encoder rotation in degree / second
+      Encoder_vx = (int)((EncoderX.getPulses() / Encoder_PPR) * 360) - Encoder_x;
+      Encoder_vy = (int)((EncoderY.getPulses() / Encoder_PPR) * 360) - Encoder_y;
+      
+      // calculate the Encoder rotation in degree
+      Encoder_x = (int)(EncoderX.getPulses() / Encoder_PPR) * 360;
+      Encoder_y = (int)(EncoderY.getPulses() / Encoder_PPR) * 360;
+
       t1.reset(); // reset the clock to time it
       t1.start();
     }
